e6b133ec09c5f278aef934e345e0a245538f8027
paulmcn98 <59029934+paulmcn98@users.noreply.github.com>
2022-05-11 16:45:25 +0100

    updates

--- marketplace/gauge-with-bands/index.js
+++ marketplace/gauge-with-bands/index.js
@@ -515,9 +515,18 @@ checkPreviewMode();
 function checkPreviewMode() {
   const queryString = window.location.href;
 
-  if (queryString === "https://davra.github.io/marketplace/gauge-with-bands/index.html" || queryString.substring(queryString.length-19,queryString.length) !== "context=connecthing") {
+  if (
+    queryString ===
+    "https://davra.github.io/marketplace/gauge-with-bands/index.html" ||
+    queryString.substring(queryString.length - 19, queryString.length) !==
+    "context=connecthing"
+  ) {
     previewMode = true;
-    vueInstance.$emit('update', { deviceId: null, metrics: null, timerange: null, chartCfg: null });
+    vueInstance.$emit("update", {
+      deviceId: null,
+      metrics: null,
+      timerange: null,
+      chartCfg: null,
+    });
   }
-
 }
